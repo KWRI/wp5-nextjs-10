c082215f44305e61e70e457652d8acd2492d28d6
Alexander Gaputin <Alexander.Gaputin@kw.com>
2021-12-08 10:28:56 -0800

    missed folder

--- home/components/Header.jsx
+++ home/components/Header.jsx
@@ -1,14 +1,33 @@
-const Header = () => (
-  <div
-    style={{
-      backgroundColor: "green",
-      color: "white",
-      width: "100%",
-      padding: "1em",
-    }}
-  >
-    <h1>Header</h1>
-  </div>
-);
+import { useState } from 'react';
+
+const Header = (props ) => {
+
+  let handleSubmit = props.handleSubmit;
+
+  if(!props.handleSubmit) {
+    handleSubmit = (event) => {
+      event.preventDefault()
+      const data = new FormData(event.target);
+      alert(`from home component: ${data.get('search')}`)
+    }
+  }
+
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        color: "white",
+        width: "100%",
+        padding: "1em",
+      }}
+    >
+      <h1>{props.title || "Header"}</h1>
+      <form id="myForm" onSubmit={handleSubmit}>
+        <input type="text" name="search"/>
+        <input type="submit"/>
+      </form>
+    </div>
+  )
+};
 
 export default Header;
